f7bbbad0dcea9ee8314580e6a93fc818458483e7
Mohamed Azzam <47927047+MohamedAzam@users.noreply.github.com>
2020-01-08 19:39:49 +0200

    Add files via upload

---
+++ Hire Me/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+	'use strict';
+	$('.Featured-Work ul li').on('click', function () {
+		$(this).addClass('active').siblings().removeClass('active');
+		
+		if ($(this).data('class') === 'all') {
+			$('.shuffle-imgs .col-md').css('opacity', 1);
+		} else {
+			$('.shuffle-imgs .col-md').css('opacity', '.08');
+			$($(this).data('class')).parent().css('opacity', 1);
+		}
+	
+	});
+});
